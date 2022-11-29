1015f2e912719fb4b19130de00490c0a15ed3330
Caelen <caelenmensch@gmail.com>
2022-11-29 11:51:59 -0800

    basic canvas

--- pages/mood.js
+++ pages/mood.js
@@ -1,5 +1,19 @@
+import { Canvas } from '@react-three/fiber'
+
+import styles from '../styles/Mood.module.css'
+
 function Mood() {
-    return <h1>MOOD</h1>
+  return (
+    <div className={styles.container}>
+      <Canvas>
+        <ambientLight intensity={2} />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial color='blue'/>
+        </mesh>
+      </Canvas>
+    </div>
+  )
 }
 
 export default Mood
