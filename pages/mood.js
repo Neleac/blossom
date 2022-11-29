15dbff1272613b3fce7feda34204f849e5a71335
Caelen <caelenmensch@gmail.com>
2022-11-29 12:08:59 -0800

    render avatar

--- pages/mood.js
+++ pages/mood.js
@@ -1,16 +1,18 @@
+import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 
 import styles from '../styles/Mood.module.css'
+
+import { Avatar } from '../src/avatar'
 
 function Mood() {
   return (
     <div className={styles.container}>
       <Canvas>
-        <ambientLight intensity={2} />
-        <mesh>
-          <boxGeometry />
-          <meshStandardMaterial color='blue'/>
-        </mesh>
+        <ambientLight />
+        <Suspense fallback={null}>
+          <Avatar />
+        </Suspense>
       </Canvas>
     </div>
   )
